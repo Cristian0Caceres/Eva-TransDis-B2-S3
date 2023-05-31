501f14aef54ad36a86b0c8b6c2321f0f18381942
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-05-31 15:44:21 -0400

    Update pyworck.py

--- pyworck.py
+++ pyworck.py
@@ -1,1 +1,4 @@
 #plano inclinado 
+angulo=int(input("ingrese el valor de uno de los angulos"))
+masa=int(input("ingrese el valor de la masa del objeto"))
+coero=int(input("ingrese el coeficiente de roce"))
