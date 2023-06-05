63aaefc4cb016fa004675c98170d2aa2684c15c4
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-06-04 21:51:57 -0400

    actualizacion 0 aceleracion sin roce
    en esta actualizacion agregamos todo lo que tiene que ver con la aceleracion sin roce para mas adelante agregar el roce y graficar

--- pyworck.py
+++ pyworck.py
@@ -1,4 +1,16 @@
 #plano inclinado 
-angulo=int(input("ingrese el valor de uno de los angulos"))
+import math
+angulo=float(input("ingrese el valor de uno de los angulos"))
 masa=int(input("ingrese el valor de la masa del objeto"))
 coero=int(input("ingrese el coeficiente de roce"))
+Gravedad=9.8
+peso=float(masa * Gravedad)
+print(peso)
+radianes=(angulo*math.pi)/180
+seno=math.sin(radianes)
+print(seno+0.00000000000000006)
+coseno=math.cos(angulo)
+newtons=float(seno*peso)
+print(newtons+0.00000000000001)
+aceleracion=float(newtons/masa)
+print(aceleracion+0.0000000000000005)
