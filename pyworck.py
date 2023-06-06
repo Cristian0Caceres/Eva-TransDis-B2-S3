eb2b7f1f04aeae4f91780e27b6af56a55046db6e
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-06-06 18:00:28 -0400

    actualizacion agrego de funcion que calcula la aceleracion con roce
    en esta actualizacion agrege una funcion la cual calcula la aceleracion con roce por separado de la sin roce

--- pyworck.py
+++ pyworck.py
@@ -7,11 +7,36 @@ def calculo_aceleracion_sin_roce():
     peso=float(masa * g)
     radianes=(ang*math.pi)/180
     seno=math.sin(radianes)+0.00000000000000006
-    coseno=math.cos(radianes)
-    print(coseno)
+
     pesox=float(seno*peso)+0.00000000000001
     aceleracion=float(pesox/masa) + 0.0000000000000005
     print(aceleracion)
     return
 
+def calculo_aceleracion_con_roce():
+    import math
+    ang=float(input("ingrese el valor de uno de los angulos"))
+    masa=int(input("ingrese el valor de la masa del objeto"))
+    mu=float(input("ingrese el coeficiente de roce"))
+    g=9.8
+    peso=float(masa * g)
+    #print(f"peso:{peso}")
+    radianes=(ang*math.pi)/180
+    sen=math.sin(radianes)+0.00000000000000006
+    cos=math.cos(radianes)
+    pesox=float(sen*peso)+0.00000000000001
+    pesoy=float(cos*peso)
+    N = pesoy
+    roce=float(N*mu)
+    Fneta=(pesox-roce)
+    roce=float(N*mu)
+    #print(f"sen es {sen}")
+    #print(f"cos es {cos}")
+    #print(f"pesox es {pesox}")
+    #print(f"pesoy es:{pesoy}")
+    aceleracion=float(Fneta/masa)
+    print(aceleracion+0.0000000000000005)
+    return
+
+calculo_aceleracion_con_roce()
 calculo_aceleracion_sin_roce()
