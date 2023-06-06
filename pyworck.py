4891c098256cae50aa35d4b4c73eff5e174b7107
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-06-06 17:26:25 -0400

    funcion aceleracion sin roce
    actualice la disposision del codigo para establecerlo dentro de una funcion dejando el codigo levemente mas ordenado y unas variables similares a las que todos estamos usando para que mas addelante no tener errores de compatibilidad

--- pyworck.py
+++ pyworck.py
@@ -1,18 +1,17 @@
 #nucleeo
 import math
-angulo=float(input("ingrese el valor de uno de los angulos"))
-masa=int(input("ingrese el valor de la masa del objeto"))
-coero=int(input("ingrese el coeficiente de roce"))
-Gravedad=9.8
-peso=float(masa * Gravedad)
-print(peso)
-radianes=(angulo*math.pi)/180
-seno=math.sin(radianes)
-print(seno+0.00000000000000006)
-coseno=math.cos(angulo)
-newtons=float(seno*peso)
-print(newtons+0.00000000000001)
-aceleracion=float(newtons/masa)
-print(aceleracion+0.0000000000000005)
+def calculo_aceleracion_sin_roce():
+    ang=float(input("ingrese el valor de uno de los angulos"))
+    masa=int(input("ingrese el valor de la masa del objeto"))
+    g=9.8
+    peso=float(masa * g)
+    radianes=(ang*math.pi)/180
+    seno=math.sin(radianes)+0.00000000000000006
+    coseno=math.cos(radianes)
+    print(coseno)
+    pesox=float(seno*peso)+0.00000000000001
+    aceleracion=float(pesox/masa) + 0.0000000000000005
+    print(aceleracion)
+    return
 
-#aqui va la formula de roce ->
+calculo_aceleracion_sin_roce()
