8db67390c4f2652c6eb4d35768268d5d924c99c0
benjamin-suarez-ascii <benjaminsuarezconus@gmail.com>
2023-06-06 18:38:21 -0400

    Cambio completo de la estructura del codigo base
    Realice un cambio completo respecto a la estructura para que sea mas simple y menos engorroso, a su vez añadi comentarios para explicar el funcionamiento del codigo

--- pyworck.py
+++ pyworck.py
@@ -1,22 +1,41 @@
-#nucleeo
 import math
-ang=float(input("ingrese el valor de uno de los angulos: "))
-masa=float(input("ingrese el valor de la masa del objeto: "))
-mu=float(input("ingrese el coeficiente de roce: "))
-g=9.8
-peso=(masa * g)
-print(f"peso:{peso}")
-radianes=(ang*math.pi)/180
-sen=math.sin(radianes)
-cos=math.cos(radianes)
-pesox=(sen*peso)
-pesoy=(cos*peso)
-N = pesoy
-roce=(N*mu)
-Fneta = (pesox-roce)
-print(f"sen es {sen}")
-print(f"cos es {cos}")
-print(f"pesox es {pesox}")
-print(f"pesoy es {pesoy}")
-aceleracion=round((Fneta/masa),2)
+
+def calcular_aceleracion(angulo, masa, coeficiente_roce=0):
+    # Gravedad
+    g = 9.8
+    # Calcular el peso del objeto
+    peso = masa * g
+    # Convertir el ángulo a radianes
+    radianes = (angulo * math.pi) / 180
+    # Calcular el seno y el coseno del ángulo
+    seno = math.sin(radianes)
+    coseno = math.cos(radianes)
+    # Calcular las componentes x e y del peso
+    pesox = seno * peso
+    pesoy = coseno * peso
+
+    if coeficiente_roce == 0:
+        # Si no hay roce, la aceleración es la componente x del peso dividida por la masa
+        aceleracion = pesox / masa
+    else:
+        # Calcular la normal (componente y del peso)
+        N = pesoy
+        # Calcular la fuerza de roce
+        roce = N * coeficiente_roce
+        # Calcular la fuerza neta (componente x del peso menos la fuerza de roce)
+        Fneta = pesox - roce
+        # Calcular la aceleración con roce
+        aceleracion = Fneta / masa
+
+    return aceleracion, masa, angulo
+
+# Solicitar los datos al usuario
+angulo = float(input("Ingrese el valor de uno de los ángulos: "))
+masa = int(input("Ingrese el valor de la masa del objeto: "))
+coeficiente_roce = float(input("Ingrese el coeficiente de roce (si no hay roce, ingrese 0): "))
+
+# Calcular la aceleración utilizando la función calcular_aceleracion()
+aceleracion = calcular_aceleracion(angulo, masa, coeficiente_roce)
+
+# Imprimir el resultado
 print(aceleracion)
