c10e077e1973a872ae9bfdd3058cb5f275fa1672
DavidGiusti <davidkontac@gmail.com>
2023-06-07 14:31:03 -0400

    formula con roce
    hay que intervenir  con los parametros

--- pyworck.py
+++ pyworck.py
@@ -16,3 +16,31 @@ aceleracion=float(newtons/masa)
 print(aceleracion+0.0000000000000005)
 
 ##
+#FORMULAS CON ROCE
+
+# Conversión de grados a radianes
+angulo_rad = math.radians(angulo)
+
+# Cálculo de la fuerza normal y el coeficiente de rozamiento
+peso_y = peso * math.cos(angulo_rad)
+
+# Cálculo de la fuerza de fricción
+fuerza_friccion = coeficiente_roce * peso_y
+
+# Impresión de resultados
+print("Fuerza normal:", peso_y)
+print("Coeficiente de rozamiento:", coeficiente_roce)
+print("Fuerza de fricción:", fuerza_friccion)
+
+# Gráfico del plano inclinado
+x = np.array([0, math.sin(angulo_rad)])
+y = np.array([0, math.cos(angulo_rad)])
+
+plt.plot(x, y)
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.xlabel("Distancia horizontal")
+plt.ylabel("Distancia vertical")
+plt.title("Plano inclinado")
+plt.show()
+
