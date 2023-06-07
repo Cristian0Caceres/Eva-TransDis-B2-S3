a2e2a52ea63212624313fc8ee40fd398c201d0f7
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-06-07 15:01:31 -0400

    actualizacion 5 creacion de buble y elecciones
    aprobechando que ahora los calculos esta en funciones genere un if el cual llame a cada función según lo decida el usuario además de generar un siclo while para que el código se ejecute las veces que el usuario requiera

--- pyworck.py
+++ pyworck.py
@@ -1,7 +1,7 @@
-#nucleeo
-import math
+import math 
+verdad=True
 def calculo_aceleracion_sin_roce():
-    ang=float(input("ingrese el valor de uno de los angulos"))
+    ang=float(input("ingrese el valor de uno de los angulos en caso del angulo ser decimal use [.] enves de [,]"))
     masa=int(input("ingrese el valor de la masa del objeto"))
     g=9.8
     peso=float(masa * g)
@@ -14,10 +14,9 @@ def calculo_aceleracion_sin_roce():
     return
 
 def calculo_aceleracion_con_roce():
-    import math
-    ang=float(input("ingrese el valor de uno de los angulos"))
+    ang=float(input("ingrese el valor de uno de los angulos en caso del angulo ser decimal use [.] enves de [,]"))
     masa=int(input("ingrese el valor de la masa del objeto"))
-    mu=float(input("ingrese el coeficiente de roce"))
+    mu=float(input("ingrese el coeficiente de roce en caso del coeficiente de roce ser decimal use [.] enves de [,]"))
     g=9.8
     peso=float(masa * g)
     #print(f"peso:{peso}")
@@ -29,7 +28,6 @@ def calculo_aceleracion_con_roce():
     N = pesoy
     roce=float(N*mu)
     Fneta=(pesox-roce)
-    roce=float(N*mu)
     #print(f"sen es {sen}")
     #print(f"cos es {cos}")
     #print(f"pesox es {pesox}")
@@ -37,6 +35,21 @@ def calculo_aceleracion_con_roce():
     aceleracion=float(Fneta/masa)
     print(aceleracion+0.0000000000000005)
     return
-
-calculo_aceleracion_con_roce()
-calculo_aceleracion_sin_roce()
+while verdad==True:
+    print("bienvenido al prototipo de calculo de plano inclinado ahora sin animaciones")
+    print("inserte 1 si desea calcular con roce inserte 2 si desea calcular sin roce")
+    eleccion=input("inserte su numero aqui: ")
+    if eleccion=="1":
+        calculo_aceleracion_con_roce()
+        pregunta=input("desea continar?: ")
+        if pregunta=="si":
+            continue
+        if pregunta=="no":
+            verdad=False
+        if eleccion=="2":
+            calculo_aceleracion_sin_roce()
+        pregunta=input("desea continar?: ")
+        if pregunta=="si":
+            continue
+        if pregunta=="no":
+            verdad=False
