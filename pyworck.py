70351e684eedd25653c17c454db33d3ed3173e60
benjamin-suarez-ascii <benjaminsuarezconus@gmail.com>
2023-06-14 14:19:35 -0400

    Agregado de formulas
    Se agregaron las formulas para el entendimiento de los calculos que hizo el programa de la simulacion

--- pyworck.py
+++ pyworck.py
@@ -67,10 +67,10 @@ def calcular_button_click():
         resultado_text.set(f"Aceleración: {aceleracion}\n"
                            f"Masa: {masa_objeto}\n"
                            f"Ángulo: {angulo_objeto}\n"
-                           f"Fuerza de Roce: {roce}\n"
-                           f"Fuerza Neta: {fuerza_neta}\n"
-                           f"Peso en X: {pesox}\n"
-                           f"Peso en Y: {pesoy}")
+                           f"Fuerza de Roce = (N*μ) = {roce}\n"
+                           f"Fuerza Neta: P + N + Fr = {fuerza_neta}\n"
+                           f"Peso en X = P * sen(α) = {pesox}\n"
+                           f"Peso en Y = P * cos(α) = {pesoy}")
         
         dibujar_triangulo(angulo)
 
