f6ec15087c5859dd11105de65a0aadfccb2c33ef
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-06-22 08:17:46 -0400

    modificacion de codigo para trabajar en una version similar a el codigo base
    actualice mi codigo en base a la raiz principall para comensar a trabajar en detalles importantes dentro de la simulacion

--- pyworck.py
+++ pyworck.py
@@ -1,38 +1,61 @@
+#Se importan las librerias necesarias para realizar el codigo.
 import math
-import pygame as py
 import tkinter as tk
-import sys
 from tkinter import messagebox
+#
+#Se crea una funcion en la cual se ejecuten las formulas necesarias en el plano inclinado.
 def calcular_aceleracion(angulo, masa, coeficiente_roce=0):
-    # Gravedad
     g = 9.8
-    # Calcular el peso del objeto
     peso = masa * g
-    # Convertir el ángulo a radianes
     radianes = (angulo * math.pi) / 180
-    # Calcular el seno y el coseno del ángulo
     seno = math.sin(radianes)
     coseno = math.cos(radianes)
-    # Calcular las componentes x e y del peso
     pesox = seno * peso
     pesoy = coseno * peso
 
-
+#Se especifican los limites y si se salen de esto, genere un error.
     if coeficiente_roce < 0 or coeficiente_roce > 1:
         raise ValueError("El coeficiente de roce debe estar entre 0 y 1")
     if angulo < 1 or angulo > 90:
         raise ValueError("El angulo debe estar entre 1 y 89 grados")
-    # Calcular la normal (componente y del peso)
+    
     N = pesoy
-    # Calcular la fuerza de roce
     roce = round(N * coeficiente_roce, 2)
     # Calcular la fuerza neta (componente x del peso menos la fuerza de roce)
     fuerza_neta = round(pesox - roce, 2)
+    fuerza_neta = max(fuerza_neta, 0)
     # Calcular la aceleración con roce
     aceleracion = round(fuerza_neta / masa, 2)
 
     return aceleracion, round(masa, 2), angulo, roce, fuerza_neta, round(pesox, 2), round(pesoy, 2)
 
+#Se crea una funcion en la cual se centrara en la visualizacion del triangulo rectangulo del plano inclinado.
+def dibujar_triangulo(angulo):
+    canvas.delete("triangulo")  # Borra cualquier triángulo dibujado previamente
+
+    #Convertir el ángulo a radianes
+    radianes = math.radians(angulo)
+
+    #Coordenadas del vértice del ángulo agudo
+    x1 = 40
+    y1 = 450
+
+    #Longitud del cateto adyacente y cateto opuesto
+    cateto_adyacente = 420
+    cateto_opuesto = cateto_adyacente * math.tan(radianes)
+
+    #Coordenadas del segundo vértice (en el cateto adyacente)
+    x2 = x1 + cateto_adyacente
+    y2 = y1
+
+    #Coordenadas del tercer vértice (en el cateto opuesto)
+    x3 = x1
+    y3 = y1 - cateto_opuesto
+
+    #Dibujar el triángulo en el lienzo
+    canvas.create_polygon(x1, y1, x2, y2, x3, y3, outline="black", fill="lightblue", tags="triangulo")
+
+#Se crea una funcion que se centrara en la ventana de Tkinter en donde el usuario podra ingresar los datos.
 def calcular_button_click():
     try:
         angulo = float(angulo_entry.get())
@@ -44,59 +67,49 @@ def calcular_button_click():
         resultado_text.set(f"Aceleración: {aceleracion}\n"
                            f"Masa: {masa_objeto}\n"
                            f"Ángulo: {angulo_objeto}\n"
-                           f"Fuerza de Roce: {roce}\n"
-                           f"Fuerza Neta: {fuerza_neta}\n"
-                           f"Peso en X: {pesox}\n"
-                           f"Peso en Y: {pesoy}")
+                           f"Fuerza de Roce = (N x μ) = {roce}\n"
+                           f"Fuerza Neta: P + N + Fr = {fuerza_neta}\n"
+                           f"Peso en X = P x sen(α) = {pesox}\n"
+                           f"Peso en Y = P x cos(α) = {pesoy}")
+        
+        dibujar_triangulo(angulo)
+
     except ValueError as e:
         messagebox.showerror("Error", str(e))
 
-# Crear la ventana principal
+#Crear la ventana principal
 window = tk.Tk()
 window.title("Cálculo de Aceleración")
 
-# Etiqueta y campo de entrada para el ángulo
+#Etiqueta y campo de entrada para el ángulo
 angulo_label = tk.Label(window, text="Ángulo:")
 angulo_label.pack()
 angulo_entry = tk.Entry(window)
 angulo_entry.pack()
 
-# Etiqueta y campo de entrada para la masa
+#Etiqueta y campo de entrada para la masa
 masa_label = tk.Label(window, text="Masa:")
 masa_label.pack()
 masa_entry = tk.Entry(window)
 masa_entry.pack()
 
-# Etiqueta y campo de entrada para el coeficiente de roce
+#Etiqueta y campo de entrada para el coeficiente de roce
 coeficiente_roce_label = tk.Label(window, text="Coeficiente de Roce:")
 coeficiente_roce_label.pack()
 coeficiente_roce_entry = tk.Entry(window)
 coeficiente_roce_entry.pack()
 
-# Botón de cálculo
+#Botón de cálculo
 calcular_button = tk.Button(window, text="Calcular", command=calcular_button_click)
 calcular_button.pack()
 
-# Variable de texto para mostrar el resultado
+#Variable de texto para mostrar el resultado
 resultado_text = tk.StringVar()
 resultado_label = tk.Label(window, textvariable=resultado_text)
 resultado_label.pack()
 
-# Iniciar el bucle principal de la interfaz gráfica
-window.mainloop()
+canvas = tk.Canvas(window, width=500, height=500)
+canvas.pack()
 
-py.init()
-
-width=640
-height=480
-screen=py.display.set_mode((width,height))
-
-py.display.set_caption("My Mame")
-
-while True:
-    for event in py.event.get():
-        if event.type==py.quit:
-            py.quit()
-            sys.exit()
-
-    py.display.update()
+#Iniciar el bucle principal de la interfaz gráfica
+window.mainloop() 
